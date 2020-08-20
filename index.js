488332b640ac53733682b4da7d50967e25af2a94
bprvuyyuru <b.p.r.vamsi07@gmail.com>
2020-08-20 15:10:42 -0400

    status1

---
+++ index.js
@@ -0,0 +1,58 @@
+const robotimg = document.querySelector(".codeimg");
+
+robotimg.style.animation = "shakerobot 100s ease";
+
+
+
+
+function upload1() {
+  document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
+    });
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
+    });
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
+    });
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
+        dropZoneElement.classList.remove("drop-zone--over");
+      });
+    });
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+}
+
+
+
+
+///////////navbar script////
+// window.onscroll = function () {
+//   myFunction()
+// };
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
